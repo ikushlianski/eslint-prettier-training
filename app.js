e5e6a1d0ef7ab7485cd36829825da0af9b0b8115
Ilya Kushlianski <kushliansky@gmail.com>
2019-03-31 00:11:58 +0300

    Try airbnb eslint config

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 var foo = bar;
+
+const a = (...arr) => {
+  let b = 5;
+
+  return arr.filter(x => x === b);
+};
+
+function something(arg1, arg2,) {
+  if (true) {
+    const c = 5;
+
+    return 5 && c;
+  }
+
+  something(arg1 + 1, 4);
+}
+
+something();
