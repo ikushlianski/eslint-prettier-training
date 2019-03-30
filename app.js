5d86067fc0fcb676ef89d4708815420944d6404a
Ilya Kushlianski <kushliansky@gmail.com>
2019-03-31 02:53:31 +0300

    Testing prettier dangling comma and prettier hooks

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-var fooBar = bar
+var fooBar = bar;
 
 const a = (...arr) => {
-    let b = 5
+    let b = 5;
 
-    return arr.filter(x => x === b)
-}
+    return arr.filter(x => x === b);
+};
 
 function something(arg1, arg2) {
     if (fooBar == 9) {
-        let c = fooBar === 3 ? 4 : 3
+        let c = fooBar === 3 ? 4 : 3;
 
-        console.warn("something")
+        console.warn("something");
 
-        const arr = ["something", "someone"]
+        const arr = [
+            "something",
+            "someone",
+            "someone else",
+            "some long string here too",
+        ];
 
         return {
             line: 5 && c,
             flagged: true,
-        }
+        };
     }
 
     something(
         "some very long string argument",
         "some very very very long string argument 2"
-    )
+    );
 }
 
-something()
+something();
